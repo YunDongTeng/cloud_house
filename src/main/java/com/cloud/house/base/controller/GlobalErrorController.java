294a60da67f8994eb3e239fade9182b6015f2bb9
日落北极 <tyd0437@163.com>
2019-10-14 08:40:02 +0800

    kk

--- src/main/java/com/cloud/house/base/controller/GlobalErrorController.java
+++ src/main/java/com/cloud/house/base/controller/GlobalErrorController.java
@@ -4,7 +4,6 @@ import com.cloud.house.base.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.boot.autoconfigure.web.ErrorController;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
